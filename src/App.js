9ef03a359cab8d263517ab6dc39ce82132887b9b
Zachary Lowy <Zachl@itsomw.com>
2023-12-14 13:23:00 -0500

    First Edit and added netlify toml

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+JS
+
+import React from 'react';
 import './App.css';
+import PiggyLogo from './Piggylogo.png'; // Importing the image
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={PiggyLogo} alt="Logo" className="App-logo" />
       </header>
+      <section className="App-content">
+        <h2>The Future of Logistics</h2>
+      </section>
+      <section className="App-coming-soon">
+        <p>Coming Soon...</p>
+      </section>
+      <footer className="App-footer">
+        <p>© Piggybackit 2024</p>
+      </footer>
     </div>
   );
 }
