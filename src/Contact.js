2a007dd8c64b6b4d98647c5ff5d5def2e2f40154
Zachary Lowy <Zachl@itsomw.com>
2024-01-02 17:37:02 -0500

    updated buttons

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
 
 function Contact() {
   return (
     <div className="contact-container">
       <h1 className="contact-title">Contact Us</h1>
-      <p className="contact-content">Have questions? We'd love to hear from you. Send us a message and we'll respond as soon as possible.</p>
+      <p className="contact-content">
+        Have questions? We'd love to hear from you. Send us a message and we'll
+        respond as soon as possible.
+      </p>
       {/* You can add a contact form or additional information here */}
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import './Home.css';
-import PhoneImage from './piggyphone.png'; 
+import React from "react";
+import "./Home.css";
+import PhoneImage from "./piggyphone.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faApple, faAndroid } from '@fortawesome/free-brands-svg-icons';
 
 function Home() {
   return (
     <div className="home-container">
       <div className="home-text">
         <h1 className="home-title">Welcome to Piggyback</h1>
-        <p className="home-content">Piggyback is a convenient app that connects individuals who are traveling with those in need of package delivery services. Whether you're heading to work, running errands, or going on a trip, you can earn extra money by picking up and dropping off packages along your route. Simply sign up, browse available delivery requests, choose the ones that suit your journey, and earn on-the-go. Senders can post their package details, and drivers can communicate with them for smooth coordination. Make package delivery a part of your daily routine with Piggyback.</p>
+        <p className="home-content">
+          Piggyback is a convenient app that connects individuals who are
+          traveling with those in need of package delivery services. Whether
+          you're heading to work, running errands, or going on a trip, you can
+          earn extra money by picking up and dropping off packages along your
+          route. Simply sign up, browse available delivery requests, choose the
+          ones that suit your journey, and earn on-the-go. Senders can post
+          their package details, and drivers can communicate with them for
+          smooth coordination. Make package delivery a part of your daily
+          routine with Piggyback.
+        </p>
         <div className="download-buttons">
-          <button className="download-button">Download for iOS</button>
-          <button className="download-button">Download for Android</button>
+          <button className="download-button">
+            <FontAwesomeIcon icon={faApple} className="icon" /> Download for iOS
+          </button>
+          <button className="download-button">
+            <FontAwesomeIcon icon={faAndroid} className="icon" /> Download for Android
+          </button>
         </div>
       </div>
       <div className="home-image">
