85812f97a0581dc54fbe9146b7781bb0fed8e31d
Zachary Lowy <Zachl@itsomw.com>
2024-01-05 01:06:49 -0500

    deploy form

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,43 +1,31 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
+import './ContactForm.css'
+
 function ContactForm() {
   const [state, handleSubmit] = useForm("xbjnjppo");
   if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
+      return <p className="form-success-message">Thanks for joining!</p>;
   }
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
+    <form onSubmit={handleSubmit} className="contact-form">
+      <div className="form-field">
+        <label htmlFor="email">Email Address</label>
+        <input id="email" type="email" name="email" />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+      </div>
+
+      <div className="form-field">
+        <label htmlFor="message">Message</label>
+        <textarea id="message" name="message" />
+        <ValidationError prefix="Message" field="message" errors={state.errors} />
+      </div>
+
+      <button type="submit" disabled={state.submitting} className="submit-button">
         Submit
       </button>
     </form>
   );
 }
-function App() {
-  return (
-    <ContactForm />
-  );
-}
-export default App;
+
+export default ContactForm;
