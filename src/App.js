36a9d5f34aa433737704e481bd8b321c338e4495
Zachary Lowy <Zach@itstrademate.com>
2024-02-15 15:23:52 -0500

    Created a secret Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './NavigationBar';
-import Home from './Home'; 
-import Footer from './Footer'; 
+import Home from './Home';
+import Footer from './Footer';
 import ContactForm from './ContactForm';
+import StripeFinished from './StripeSubmitted';
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contactform" element={<ContactForm />} />
+        <Route path="/stripefinished" element={<StripeFinished />} /> {/* Fixed this line */}
       </Routes>
-      <Footer /> 
+      <Footer />
     </Router>
   );
 }
---
+++ src/StripeSubmitted.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './StripeSubmitted.css'; // Import a CSS file for styling
+
+function StripeFinished() {
+  return (
+    <div className="stripe-finished-container">
+      <h1>Thank You!</h1>
+      <p>Thank you for submitting your information. We will get back to you as soon as possible.</p>
+    </div>
+  );
+}
+
+export default StripeFinished;
